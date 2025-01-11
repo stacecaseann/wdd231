70727bb480ff11d4e5004cac2ab33ae2c25ad8f1
Stacy Yarrington <syarrington@enuit.com>
2025-01-10 23:13:52 -0700

    Week 1 home page

---
+++ scripts/dates.js
@@ -0,0 +1,7 @@
+const yearSpan = document.querySelector(".year")
+const date = new Date();
+const year = date.getFullYear();
+yearSpan.textContent = year;
+
+const lastModified = document.getElementById("lastModified");
+lastModified.innerHTML = `Last Modified: ${document.lastModified}`
